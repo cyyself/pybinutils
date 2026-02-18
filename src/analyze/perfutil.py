17672a1998475839a517498bd8886287f577f403
Yangyu Chen <cyy@cyyself.name>
2026-02-19 00:58:56 +0800

    perfutil.py: extract_perf_from_file_with_symbol to use subprocess and support new format

--- src/analyze/perfutil.py
+++ src/analyze/perfutil.py
@@ -4,6 +4,7 @@ import tempfile
 import os
 import re
 import sys
+import subprocess
 
 # return: (symbol, offset)
 def split_target_symbol(symbol_addr):
@@ -62,34 +63,42 @@ def extract_perf_from_file(file, aslr_map=None):
             return res
 
 # file: perf.data
-# return: {file: {event: {symbol: count}}}
-def extract_perf_from_file_with_symbol(file):
-    with tempfile.NamedTemporaryFile() as tmp:
-        os.system("perf script -i {} --no-demangle --full-source-path > {}".format(file, tmp.name))
-        with open(tmp.name, 'r') as f:
-            lines = f.readlines()
-            res = dict()
-            for line in lines:
-                colon_pos = line.find(":")
-                if colon_pos == -1:
-                    continue
-                line_split = line[colon_pos+1:].split()
-                freq = int(line_split[0])
-                event = line_split[1][:-1]
-                symbol = line_split[3]
-                if symbol == '[unknown]':
-                    continue
-                if '+' in symbol:
-                    symbol = symbol.split('+')[0]
-                file = line_split[-1].strip()[1:-1]
-                if file not in res:
-                    res[file] = dict()
-                if event not in res[file]:
-                    res[file][event] = dict()
-                if symbol not in res[file][event]:
-                    res[file][event][symbol] = 0
-                res[file][event][symbol] += freq
-            return res
+# return: {file: {event: {symbol: count}}} (has_symbol_offset = False)
+# return: {file: {event: {symbol: {offset: count}}}} (has_symbol_offset = True)
+def extract_perf_from_file_with_symbol(file, has_symbol_offset=False):
+    result = subprocess.run(["perf", "script", "-i", file, "--no-demangle", "--full-source-path"], capture_output=True, text=True)
+    lines = result.stdout.splitlines()
+    res = dict()
+    for line in lines:
+        colon_pos = line.find(":")
+        if colon_pos == -1:
+            continue
+        line_split = line[colon_pos+1:].split()
+        freq = int(line_split[0])
+        event = line_split[1][:-1]
+        symbol = line_split[3]
+        if symbol == '[unknown]':
+            continue
+        if '+' in symbol:
+            symbol = symbol.split('+')[0]
+        file = line_split[-1].strip()[1:-1]
+        if file not in res:
+            res[file] = dict()
+        if event not in res[file]:
+            res[file][event] = dict()
+        if symbol not in res[file][event]:
+            if has_symbol_offset:
+                res[file][event][symbol] = dict()
+            else:
+                res[file][event][symbol] = 0
+        if has_symbol_offset:
+            offset = int(line_split[4], 16) if len(line_split) > 4 else 0
+            if offset not in res[file][event][symbol]:
+                res[file][event][symbol][offset] = 0
+            res[file][event][symbol][offset] += freq
+        else:
+            res[file][event][symbol] += freq
+    return res
 
 def perf_extract_deaslr_per_file(perf_extract_file, aslr_map_file, textdump):
     # de-aslr perf data
